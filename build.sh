b7beb588d5408227ded6456c9d61f3847cbbe59e
Daniel Shin <kyuseung1016@gmail.com>
2024-05-27 18:12:05 +0900

    build cmd improvements

--- build.sh
+++ build.sh
@@ -3,11 +3,21 @@
 # cmake command to build. 
 
 if [[ -z "${GRPC_CPP_INSTALL}" ]]; then
-  echo "need to set GRPC_CPP_INSTALL env var to point to gRPC installation directory."
-  exit
+    echo "need to set GRPC_CPP_INSTALL env var to point to gRPC directory."
+    echo "refer to README for more instructions."
+    exit
 fi
 
-mkdir build
+if [ -f "build" ]; then
+    echo "there is a file named 'build'."
+    exit
+elif [ ! -d "build" ]; then
+    echo "creating 'build' directory."
+    mkdir build
+else
+    echo "'build' directory found."
+fi
+
 cd build
 cmake -DCMAKE_PREFIX_PATH=$GRPC_CPP_INSTALL ..
 cmake --build .
