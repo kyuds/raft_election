e9bdfde68b60f1da7a7fe5eece210f798fa3af2b
Daniel Shin <kyuseung1016@gmail.com>
2024-06-21 15:51:19 +0900

    refactor configuration for more options

--- src/config.cc
+++ src/config.cc
@@ -1,5 +1,6 @@
 #include <fstream>
-#include "nlohmann/json.hpp"
+#include <plog/Log.h>
+#include <nlohmann/json.hpp>
 
 #include "config.h"
 #include "utils.h"
@@ -8,22 +9,91 @@ using json = nlohmann::json;
 
 namespace raft {
 
-ConfigParser::ConfigParser(const std::string& name, const std::string& config_file) {
-    std::ifstream f(config_file);
-    if (!f.is_open()) {
-        // (TODO: kyuds) error here.
-    } 
+Config::Config(const std::string& name,
+               const std::string& address,
+               const std::string& config_file)
+    : name(name), address(address) {
+    try {
+        std::ifstream f(config_file);
+        json data = json::parse(f);
 
-    json data = json::parse(f);
-
-    min_timeout = data["election"]["min_timeout"];
-    max_timeout = data["election"]["max_timeout"];
-    rpc_timeout = data["rpc"]["timeout"];
-
-    storage_dir = std::string(data["durable"]["directory"]);
-    if (data["durable"]["append_node_name"]) {
-        storage_dir = combine_paths(storage_dir, name);
+        storage_dir = std::string(data.value("/storage/directory"_json_pointer, "tmp"));
+        append_node_name = data.value("/storage/append_node_name"_json_pointer, false);
+        min_timeout = data.value("/election/min_timeout"_json_pointer, 150);
+        max_timeout = data.value("/election/max_timeout"_json_pointer, 300);
+        rpc_timeout = data.value("/rpc/timeout"_json_pointer, 100);
+        peer_file = std::string(data.value("/peer/config"_json_pointer, "peer.txt"));
+        heartbeat = data.value("/peer/heartbeat"_json_pointer, 20);
+        PLOGI << "Successfully parsed file: " << config_file;
+        PLOGI << name << " " << address;
+    } catch (const std::exception& e) {
+        PLOGE << "Error occurred in parsing config file " << config_file << " " << e.what();
     }
+}
+
+Config * Config::default_config(const std::string& name,
+                                const std::string& address) {
+    return (new Config())
+            ->set_name(name)
+            ->set_address(address)
+            ->set_peer_file("peer.txt")
+            ->set_heartbeat(20)
+            ->set_min_timeout(150)
+            ->set_max_timeout(300)
+            ->set_rpc_timeout(100)
+            ->set_storage_dir("tmp", true);
+}
+
+Config * Config::set_name(const std::string& n) {
+    name = n;
+    return this;
+}
+
+Config * Config::set_address(const std::string& a) {
+    address = a;
+    return this;
+}
+
+Config * Config::set_peer_file(const std::string& p) {
+    peer_file = p;
+    return this;
+}
+
+Config * Config::set_heartbeat(const int t) {
+    heartbeat = t;
+    return this;
+}
+
+Config * Config::set_min_timeout(const int t) {
+    min_timeout = t;
+    return this;
+}
+
+Config * Config::set_max_timeout(const int t) {
+    max_timeout = t;
+    return this;
+}
+
+Config * Config::set_rpc_timeout(const int t) {
+    rpc_timeout = t;
+    return this;
+}
+
+Config * Config::set_storage_dir(const std::string& d, bool append_name) {
+    append_node_name = append_name;
+    storage_dir = d;
+    return this;
+}
+
+const std::string& Config::get_storage_dir() {
+    if (append_node_name) {
+        if (!name.empty()) 
+            storage_dir = combine_paths(storage_dir, name);
+        else 
+            PLOGW << "Configured to add node name to storage path but node name not set. Skipping";
+        append_node_name = false; // prevent further combination
+    }
+    return storage_dir;
 }
 
 } // namespace raft
--- src/config.h
+++ src/config.h
@@ -14,7 +14,7 @@ store configurations about cluster membership as we will
 advance the protocol to allow for membership changes. 
 
 See "config.json" for more details on how the json file
-should be organized.
+should be organized if using the file configuration version.
 
 TODO:
 - figure out how to import gRPC settings.
@@ -22,28 +22,55 @@ TODO:
 
 namespace raft {
 
-class ConfigParser {
+class Config {
     public:
-        ConfigParser(const std::string& name, const std::string& config_file);
-        ~ConfigParser() {}
+        // empty Config object
+        Config() {}
+        // Config object constructed from a config_file
+        Config(const std::string& name,
+               const std::string& address,
+               const std::string& config_file);
 
+        // default configuration. Most common use case.
+        static Config * default_config(const std::string& name,
+                                       const std::string& address);
+
+        const std::string& get_name() { return name; }
+        const std::string& get_address() { return address; }
+        const std::string& get_peer_file() { return peer_file; }
+        int get_heartbeat() { return heartbeat; }
         int get_min_timeout() { return min_timeout; }
         int get_max_timeout() { return max_timeout; }
         int get_rpc_timeout() { return rpc_timeout; }
-        const std::string& get_storage_dir() { return storage_dir; }
+        const std::string& get_storage_dir();
+
+        // setters. Returns 'this' to chain functions.
+        Config * set_name(const std::string& n);
+        Config * set_address(const std::string& a);
+        Config * set_peer_file(const std::string& p);
+        Config * set_heartbeat(const int t);
+        Config * set_min_timeout(const int t);
+        Config * set_max_timeout(const int t);
+        Config * set_rpc_timeout(const int t);
+        Config * set_storage_dir(const std::string& d, bool append_name = false);
     
     private:
-        // raft protocol
-
-        // durable
-        std::string storage_dir;
-
+        // protocol
+        std::string name;
+        std::string address;
+        std::string peer_file;
+        int heartbeat; // milliseconds
+        
         // election
         int min_timeout; // milliseconds
         int max_timeout;
 
         // rpc
-        int rpc_timeout;
+        int rpc_timeout; // milliseconds
+
+        // storage
+        std::string storage_dir;
+        bool append_node_name;
 };
     
 } // namespace raft
